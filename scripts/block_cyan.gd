c3bd52ff47dbb0542aeff31fe18829a8e27686e7
cale <cdqwertz@gmail.com>
2017-02-01 12:01:45 +0100

    Add lever + Add new level (WIP)

---
+++ scripts/block_cyan.gd
@@ -0,0 +1,28 @@
+extends StaticBody2D
+
+var state = false
+var states = {}
+
+onready var collider = get_node("CollisionShape2D")
+
+func _ready():
+	set_process(true)
+	
+func _process(delta):
+	if state:
+		set_opacity(0.3)
+		collider.set_trigger(true)
+	else:
+		set_opacity(1.0)
+		collider.set_trigger(false)
+
+func set_state(name, value):
+	states[name] = value
+	update_state()
+	
+func update_state():
+	for name in states.keys():
+		if not(states[name]):
+			state = false
+			return
+	state = true
---
+++ scripts/lever.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var objects = []
+var state = false
+
+onready var sprite = get_node("AnimatedSprite")
+
+func _ready():
+	pass
+
+func _on_area_1_body_enter(body):
+	if body.is_in_group("player"):
+		state = !state
+		
+		if state == false:
+			sprite.set_frame(0)
+		else:
+			sprite.set_frame(1)
+		
+		for object in objects:
+			object.set_state(get_name(), state)
+
+func _on_area_2_body_enter(body):
+	if body.is_in_group("actuator"):
+		objects.append(body)
+		body.set_state(get_name(), state)
+
+func _on_area_2_body_exit(body):
+	if body.is_in_group("actuator"):
+		objects.append(body)
+
