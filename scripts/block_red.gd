4359d753c200c1f1c60aeda28d787cd172e04e64
cale <cdqwertz@gmail.com>
2017-04-03 18:02:22 +0200

    Add level 13

--- scripts/block_red.gd
+++ scripts/block_red.gd
@@ -1,9 +1,17 @@
 extends Area2D
 
+export var spin = false
+export var spin_speed = -60
 onready var spawn = get_parent().get_node("spawn")
 
 func _ready():
 	connect("body_enter", self, "body_enter")
+	if spin:
+		set_process(true)
+		
+func _process(delta):
+	if spin:
+		rotate(deg2rad(spin_speed) * delta)
 	
 func body_enter(body):
 	if body.is_in_group("player"):
